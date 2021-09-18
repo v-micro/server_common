c2cb63454759ddbb6a0be89bbf629928580222c8
蔡辉 <caihui@rp-pet.cn>
2021-09-18 12:23:00 +0800

    'ok'

---
+++ go.mod
@@ -0,0 +1,8 @@
+module servert_comon
+
+go 1.13
+
+require (
+	github.com/golang/protobuf v1.5.2 // indirect
+	google.golang.org/grpc v1.40.0 // indirect
+)
---
+++ protobuf.go
@@ -0,0 +1,72 @@
+package main
+
+import (
+	"bytes"
+	"io/ioutil"
+	"os"
+	"os/exec"
+	"strings"
+)
+
+var buf []string
+
+func Protobuf(name string) error {
+	cmd := exec.Command("cmd.exe", "/C", "protoc --go_out=plugins=grpc:. "+name)
+	w := bytes.NewBuffer(nil)
+	cmd.Stderr = w
+	if err := cmd.Run(); err != nil {
+		return err
+	}
+
+	//==== 删除pb文件的 omitempty
+	pb := strings.Replace(name,".proto",".pb.go",-1)
+	_, err := PathExists(pb)
+	if err != nil {return err}
+
+	f, err := os.OpenFile(pb, os.O_RDWR, os.ModePerm);
+	if err != nil {return err}
+	defer f.Close()
+
+	b, err := ioutil.ReadAll(f);
+	if err != nil {return err}
+	n := strings.ReplaceAll(string(b), ",omitempty", "")
+	err = os.Truncate(pb, 0)
+	if err != nil {return err}
+	_, err = f.WriteAt([]byte(n), 0)
+	if err != nil {return err}
+
+	return nil
+}
+
+//遍历目录
+func GetBufFiles(folder string){
+	files, _ := ioutil.ReadDir(folder)
+	for _,file := range files{
+		if file.IsDir(){
+			GetBufFiles(folder + "/" + file.Name())
+		}else if strings.HasSuffix(file.Name(), ".proto") == true{
+			buf = append(buf,folder + "/" + file.Name())
+		}
+	}
+}
+
+//目录是否存在
+func PathExists(path string) (bool, error) {
+	_, err := os.Stat(path)
+	if err == nil {
+		return true, nil
+	}
+	if os.IsNotExist(err) {
+		return false, nil
+	}
+	return false, err
+}
+
+
+func main()  {
+
+	GetBufFiles(".")	//获取目录的protobuf文件
+	for _,v := range buf{
+		_ = Protobuf(v)
+	}
+}
